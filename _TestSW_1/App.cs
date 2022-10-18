7ea8e6c33dddfbebf7e2ea63c872d5b1b610b7db
Rageon <vnifedow@bk.ru>
2022-10-18 19:27:07 +0500

    +++

--- _TestSW_1/App.cs
+++ _TestSW_1/App.cs
@@ -7,6 +7,8 @@ namespace _TestSW_1
     public partial class App : Form
     {
 
+        double ODZ = 0.8;
+
         public App()
         {
             InitializeComponent();
@@ -35,15 +37,54 @@ namespace _TestSW_1
                     Convert.ToDouble(textBoxEps.Text)
                     );
                 chart1.Series["CalcFunc"].Points.DataBindXY(answer.x, answer.y);
-            } catch(Exception e)
+            } 
+            catch(Exception e)
             {
                 labelStatus.Text = e.Message;
             }
         }
 
+        bool RunValidation()
+        {
+            try
+            {
+            double a = Convert.ToDouble(textBoxA.Text);
+            double b = Convert.ToDouble(textBoxB.Text);
+            bool checkODZ(double lower, double upper)
+            {
+                for (double i = lower; i <= upper; i += 0.1) 
+                {
+                    if(i > (ODZ * -1) && i < ODZ)
+                    {
+                        MessageBox.Show("Диапaзон значений не входит в ОДЗ");
+                        return false;
+                    }
+                }
+                return true;
+            }
+            bool checkBordersIntegrity(double lower, double upper)
+            {
+                if(lower > upper)
+                {
+                    MessageBox.Show("Нижний предел больше верхнего");
+                }
+                return lower <= upper;
+            }
+            return checkODZ(a, b) && checkBordersIntegrity(a, b);
+            } 
+            catch(Exception e)
+            {
+                MessageBox.Show(e.Message);
+                return false;
+            }
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            DrawFunction();
+            if(RunValidation())
+            {
+                DrawFunction();
+            }
         }
     }
 }
